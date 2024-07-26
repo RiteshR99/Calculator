ce9eddbc5c50de21164abd7203a587c695ae30eb
RiteshR99 <160796518+RiteshR99@users.noreply.github.com>
2024-07-26 10:49:35 +0530

    Add files via upload

---
+++ dark_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+  colorScheme: ColorScheme.dark(
+    background: Colors.black12,
+    primary: Colors.white24!,
+    secondary: Colors.black38!,
+  )
+);
---
+++ light_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+  colorScheme: ColorScheme.light(
+    background: Colors.white,
+    primary: Colors.black87,
+    secondary: Colors.white24,
+
+  )
+);
